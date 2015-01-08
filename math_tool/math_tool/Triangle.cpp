69742902b4d6db45bed7a48545a9aeb1c20cdbd9
rtsun95 <rtsun@uwaterloo.ca>
2015-01-08 16:14:19 -0500

    Add triangle class function

--- math_tool/math_tool/Triangle.cpp
+++ math_tool/math_tool/Triangle.cpp
@@ -1,11 +1,35 @@
-#include "Triangle.h"
+#define _USE_MATH_DEFINES
 
+#include "Triangle.h"
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
 
 Triangle::Triangle()
 {
+	system("CLS");
+	point_A = read_coord('A');
+	point_B = read_coord('B');
+	point_C = read_coord('C');
 }
-
 
 Triangle::~Triangle()
 {
 }
+
+coord read_coord(char _name){
+	coord vertex;
+	cout << "Please enter the X and Y coordinate of the vertex " << _name << endl;
+	cin >> vertex.x >> vertex.y;
+	return vertex;
+}
+
+
+double angle_convert(double _radian){
+	return _radian * 180.0 / M_PI;
+}
+
--- math_tool/math_tool/Triangle.h
+++ math_tool/math_tool/Triangle.h
@@ -1,7 +1,24 @@
+struct coord{
+	double x, y;
+};
 class Triangle
 {
+	coord point_A, point_B, point_C;
+	double sideA, sideB, sideC;
+	double angleA, angleB, angleC;
+	double perimeter, area;
+
+	coord read_coord(char _name);
+	double angle_convert(double _radian);
+	
 public:
+	double getArea();
+	double getPerimeter();
 	Triangle();
 	~Triangle();
+	double getAngle();
+	double getSide();
+	void showInfo();
+
 };
 
